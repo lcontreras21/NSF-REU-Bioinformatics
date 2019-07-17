fixed and added more masking to test weights, further cleanup

models/nn_dense.py
@@ -9,12 +9,29 @@ class NN_dense(nn.Module):
 		self.fc1 = nn.Linear(gene_size, hidden_size)
 		self.relu = nn.ReLU()
 		self.fc2 = nn.Linear(hidden_size, num_labels)
-	def forward(self, gene_vec):
-		out = self.fc2(self.relu(self.fc1(gene_vec)))
+		self.mask()
+
+	def forward(self, input_vector):
+		out =  self.fc1(input_vector)
+		out = self.relu(out)
+		out = self.fc2(out)
 		out = F.log_softmax(out, dim=1)
+		
+		self.mask()
 		return out
 	def __str__(self):
 		return "Dense"
+
+	def mask(self):
+		# bias mask
+		mask = np.array([1] * hidden_size)
+		mask[weights_to_test] = 0
+		self.fc1.bias.data *= torch.FloatTensor(mask)
+
+		# weight mask
+		mask = np.array([[1] * input_size] * hidden_size)
+		mask[weights_to_test] = 0
+		self.fc1.weight.data *= torch.FloatTensor(mask)
 
 def train_dense_model():
 	train_model(NN_dense(input_size, hidden_size, output_size))

models/nn_split.py
@@ -10,24 +10,7 @@ from operator import itemgetter
 class NN_split(nn.Module):
 	def __init__(self, hidden_size, output_size):
 		super(NN_split, self).__init__()
-		self.gene_group_indicies = []
-		fc = []  
-		gene_indexes = gene_dict()
-		gene_groups = import_gene_groups()
-		for i, gene_group in enumerate(gene_groups):
-			if test_behavior and i in weights_to_test:
-				fc.append(nn.Linear(1,1))
-				fc[-1].weight = nn.Parameter(torch.FloatTensor([0]), requires_grad=False)
-				fc[-1].bias = nn.Parameter(torch.FloatTensor([0]), requires_grad=False)
-				self.gene_group_indicies.append([])
-			else:
-				group_indices = get_gene_indicies(gene_group, gene_indexes)
-				self.gene_group_indicies.append(group_indices)
-				# creates linear layers that has input
-				# of gene group size and outputs a 
-				# tensor of size 1
-				fc.append(nn.Linear(len(group_indices), 1))
-		self.linears = nn.ModuleList(fc)
+		self.linears = nn.ModuleList(self.make_linears())
 		self.relu = nn.ReLU()
 		self.fc2 = nn.Linear(hidden_size - len(weights_to_test), 2)
 
@@ -45,6 +28,23 @@ class NN_split(nn.Module):
 		# concatenate all the linear layers to make a tensor with size of gene groups
 		hidden = torch.stack(hidden, 1)
 		return hidden
+	
+	def make_linears(self):
+		self.gene_group_indicies = []
+		fc = []  
+		gene_indexes = gene_dict()
+		gene_groups = import_gene_groups()
+		for i, gene_group in enumerate(gene_groups):
+			if test_behavior and i in weights_to_test:
+				fc.append(nn.Linear(1,1))
+				fc[-1].weight = nn.Parameter(torch.FloatTensor([0]), requires_grad=False)
+				fc[-1].bias = nn.Parameter(torch.FloatTensor([0]), requires_grad=False)
+				self.gene_group_indicies.append([])
+			else:
+				group_indices = get_gene_indicies(gene_group, gene_indexes)
+				fc.append(nn.Linear(len(group_indices), 1))
+				self.gene_group_indicies.append(group_indices)
+		return fc
 
 	def split_data(self, input_vector):
 		data = input_vector.tolist()[0]

models/nn_zerow.py
@@ -12,21 +12,22 @@ class NN_zerow(nn.Module):
 		self.fc1 = nn.Linear(input_size, hidden_size)
 		self.relu = nn.ReLU()
 		self.fc2 = nn.Linear(hidden_size, output_size)
+		
+		self.make_mask()
+		self.set_weights()
 	
 	def forward(self, input_vector):
 		out = self.fc1(input_vector)
 		out = self.relu(out)
 		out = self.fc2(out)
 		out = F.log_softmax(out, dim=1)
+
+		self.set_bias()
+		self.set_weights()
 		return out
 
-	def set_weights(self):
-		self.fc1.weight.data *= self.mask
-
-	def __str__(self):
-		return "Zero-weights"
-	
-	def make_mask(self, gene_groups):
+	def make_mask(self):
+		gene_groups = import_gene_groups()
 		gene_indexer = gene_dict()
 		mask = np.array([[0] * input_size] * len(gene_groups))
 		for i, gene_group in enumerate(gene_groups):
@@ -34,10 +35,20 @@ class NN_zerow(nn.Module):
 				group_indices = []
 			else:
 				group_indices = get_gene_indicies(gene_group, gene_indexer)
-			mask[group_index][group_indices] = 1
+			mask[i][group_indices] = 1
 		mask = torch.FloatTensor(mask)
 		self.mask = mask
-	
+
+	def set_weights(self):
+		self.fc1.weight.data *= self.mask
+
+	def set_bias(self):
+		mask = np.array([1] * hidden_size)
+		mask[weights_to_test] = 0
+		self.fc1.bias.data *= torch.FloatTensor(mask)
+
+	def __str__(self):
+		return "Zero-weights"
 
 def train_zerow_model():
 	train_model(NN_zerow(input_size, hidden_size, output_size))

models/process_data.py
@@ -1,5 +1,4 @@
-### Place to store helper functions that load and process the
-### text data.
+### Place to store helper functions that load and process the text data
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F
@@ -14,71 +13,63 @@ import pickle
 from tqdm import tqdm
 import numpy as np
 from copy import deepcopy
+from collections import defaultdict
 
 # adds data to a list for training or testing. Could add an
 # even amount or uneven amount based on inputs.
 def add_to_data(data_set, tumor_max, normal_max):
-	normal_data = open(text_file_normal, "r")
-	tumor_data = open(text_file_tumor, "r")
+	normals = 0	
+	with open(text_file_normal, "r") as normal_data:
+		while normals < normal_max:
+			line = normal_data.readline().split()
+			data_set += [(line[1:-1], line[-1])]
+			normals += 1
+
 	tumors = 0
-	normals = 0
-	while tumors < tumor_max:
-		next_line = tumor_data.readline().split()
-		data_set += [(next_line[1:-1], next_line[-1])]
-		tumors += 1
-	while normals < normal_max:
-		next_line = normal_data.readline().split()
-		data_set += [(next_line[1:-1], next_line[-1])]
-		normals += 1
-	normal_data.close()
-	tumor_data.close()
+	with open(text_file_tumor, "r") as tumor_data:
+		while tumors < tumor_max:
+			line = tumor_data.readline().split()
+			data_set += [(line[1:-1], line[-1])]
+			tumors += 1
+
 
 # to be used for determining output of NN,
 # essentially our sigmoid function but other option is possible
 label_to_ix = {"Tumor": 0, "Normal": 1}
-def make_target(label, label_to_ix):
+def make_expected(label, label_to_ix):
 	return torch.LongTensor([label_to_ix[label]])
 
 # transforms a the gene list into a tensor for our NN
-def make_gene_vector(file_line):
-	data = list(map(float, file_line))
-	vec = torch.FloatTensor(data)
+def make_gene_vector(input_sample):
+	processed = list(map(float, input_sample))
+	vec = torch.FloatTensor(processed)
 	return vec.view(1, -1)
 
 # makes a dictionary for gene index to be able to connect nodes
 def gene_dict():
-	f = open(text_data, "r")
-	gene_names = f.readline().split()
-	f.close()
+	with open(text_data, "r") as f:
+		gene_names = f.readline().split()
 
-	gene_to_index = {}
-	for index, gene_name in enumerate(gene_names[1:-1]):
-		if gene_name not in gene_to_index:
-			gene_to_index[gene_name] = [index]
-	else:
-		gene_to_index[gene_name] = gene_to_index[gene_name] +[index]
-	return gene_to_index
+	gene_to_index = defaultdict(list)
+	for index, key in enumerate(gene_names[1:-1]):
+		gene_to_index[key].append(index)
+	return dict(gene_to_index)
 
 # import hallmark gene data
 def import_gene_groups():
-	f = open(text_gene_groups, "r")
-	gene_groups = []
-	for line in f:
-		gene_data = line.split()
-		gene_groups.append(gene_data[2:])
-	f.close()
+	with open(text_gene_groups, "r") as f:
+		gene_groups = [line.split()[2:] for line in f]
 	return gene_groups
 
-def get_gene_indicies(gene_group, gene_indexer):
-	f = open("text_files/gene_pairs.pickle", "rb")
-	alternate_names = pickle.load(f)
-	f.close()
+def get_gene_indicies(gene_group, gene_indexes):
+	with open("text_files/gene_pairs.pickle", "rb") as f:
+		alternate_names = pickle.load(f)
 	indices = []
 	for gene in gene_group:
 		try:
 			if gene in alternate_names.keys():
 				gene = alternate_names[gene]
-			indices += gene_indexer[gene]
+			indices += gene_indexes[gene]
 		except:
 			pass
 	indices.sort()
@@ -116,12 +107,6 @@ def train_model(model):
 	if debug:
 		print("Loaded the data ", flush=True)
 
-	if str(model) == "Zero-weights":
-		if debug:
-			print("Doing some calculations")
-		model.make_mask(gene_groups)
-		model.set_weights()
-
 	# train the model
 	if debug:
 		print("Training the model")
@@ -133,15 +118,14 @@ def train_model(model):
 			model.zero_grad()
 
 			gene_vec = make_gene_vector(instance)
-			target = make_target(label, label_to_ix)
+			expected = make_expected(label, label_to_ix)
 			
 			# get probabilities from instance
 			output = model(gene_vec)
 
 			# apply learning to the model based on the instance
-			loss = loss_function(output, target)
+			loss = loss_function(output, expected)
 			loss.backward()
-			if str(model) == "Zero-weights": model.set_weights() 
 			optimizer.step()
 
 	if debug:

test_models.py
@@ -24,24 +24,24 @@ def test_model(model, testing_data):
 	with torch.no_grad():
 		#vals = [true_pos, total_pos, true_neg, total_neg, correct, total] 
 		vals = [0] * 6
-		testing_size = normal_data_size + tumor_data_size 
+		testing_size = len(testing_data)
 		for i in tqdm(range(testing_size), disable=not debug):
 			instance, label = testing_data[i]
 			
 			gene_vec = make_gene_vector(instance)
-			target = make_target(label, label_to_ix)
+			expected = make_expected(label, label_to_ix)
 			outputs = model(gene_vec)
 			_, predicted = torch.max(outputs.data, 1)
-			vals[5] += target.size(0)
-			vals[4] += (predicted == target).item()
+			vals[5] += expected.size(0)
+			vals[4] += (predicted == expected).item()
 			
-			if target.item() == 1:  # getting normal is considered True, tumor is False
+			if expected.item() == 1:  # getting normal is considered True, tumor is False
 				vals[1] += 1
-				if torch.equal(predicted, target):
+				if torch.equal(predicted, expected):
 					vals[0] += 1  # true positive if predicted == actual
-			elif target.item() == 0:
+			elif expected.item() == 0:
 				vals[3] += 1
-				if torch.equal(predicted, target):
+				if torch.equal(predicted, expected):
 					vals[2] += 1  # true negative if predicted == actual
 			
 	# account for specificity and sensitivity here
@@ -71,8 +71,8 @@ def test_models():
 	testing_data = []
 	add_to_data(testing_data, tumor_data_size, normal_data_size)
 	random.shuffle(testing_data)
-	
-	models = [NN(input_size, hidden_size, output_size), 
+
+	models = [NN_zerow(input_size, hidden_size, output_size), 
 			NN_dense(input_size, hidden_size, output_size), 
 			NN_split(hidden_size, output_size)]
 	